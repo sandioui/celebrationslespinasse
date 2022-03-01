a88a0de96dee80df909408b0156cbe1692f2d106
sandioui <sandrine.stonge@gmail.com>
2022-03-01 15:03:02 -0500

    adding deployment

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git add -A 
+
+git commit -m 'deploy'
+
+git push -f git@github.com:sandioui/celebrationslespinasse.git main:gh-pages
+
+cd -
